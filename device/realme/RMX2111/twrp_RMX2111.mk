8dc5769ae8d2792a7b6a701fd46c4ebdf0c107e5
czesio666 <73993043+czesio666@users.noreply.github.com>
2025-03-14 23:51:23 +0100

    Update twrp_RMX2111.mk

--- device/realme/RMX2111/twrp_RMX2111.mk
+++ device/realme/RMX2111/twrp_RMX2111.mk
@@ -5,12 +5,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Release name
+PRODUCT_RELEASE_NAME := RMX2111
+DEVICE_PATH := device/realme/RMX2111
+
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from RMX2111 device
 $(call inherit-product, device/realme/RMX2111/device.mk)
